d385b6c8fdfda0b6ea4b4e0918b45fedbf21a8c7
xiegj <xiegj@loncomip.com>
2018-01-20 15:50:36 +0800

    侧边展示

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,46 +1,43 @@
 <template>
-    <div class="loncom_index">
-        zutai
+    <div class="loncom_content">
+        <div class="loncom_sidebar loncom_zt_sidebar" ref="sidebar">
+            <div class="loncom_zt_sidebarcon">
+                <div class="loncom_zt_sidebarbox loncom_mb10">
+                    <el-tabs v-model="activeName">
+                        <el-tab-pane label="设备目录" name="first">
+                            设备目录
+                        </el-tab-pane>
+                        <el-tab-pane label="设备地图" name="second">
+                            <div class="loncom_zt_map">
+                                <div class="loncom_zt_mapimg">
+                                    <img :src="'static/zutai/images/img.png'">
+                                </div>
+                                <div class="loncom_zt_map_btn">
+                                    <span><i class="fa fa-edit"></i></span>
+                                    <span><i class="fa fa-pencil"></i></span>
+                                    <span><i class="fa fa-trash"></i></span>
+                                </div>
+                            </div>
+                        </el-tab-pane>
+                    </el-tabs>
+                </div>
+                <div class="loncom_zt_sidebarbox loncom_zt_mapadd">
+                    <i class="fa fa-plus"></i>
+                </div>
+                <span class="loncom_zt_sidebar_btn" @click="navclick"><i class="fa fa-chevron-left" ref="navbtn"></i></span>
+            </div>
+        </div>
+        <div class="loncom_sidebar_right loncom_zt_sidebar_right" ref="content">
+            123
+        </div>
+        
     </div>
 </template>
 
 
-
-
-
 <script>
-
-import Vue from 'vue' 
-
-
 export default {
-  created () {
-    // this.$api.get('/base/subevent', {"Url":"menu","action":"query","model":JSON.stringify({"id":"0"})}, r => {
-    //   console.log(r);
-
-    //   if(r.err_code=="0"){
-    //     this.listData=r.data;
-    //     for(var i=0;i<this.listData.length;i++){
-    //         Vue.set(this.listData[i],'loncom_active',false);
-    //         if(this.listData[i].devlist.length>0){
-    //             var devcountall=0;
-    //             var isalarmall=0;
-    //             for(var j=0;j<this.listData[i].devlist.length;j++){
-    //                 devcountall+=this.listData[i].devlist[j].devcount;
-    //                 isalarmall+=this.listData[i].devlist[j].isalarm;
-    //             }
-    //             Vue.set(this.listData[i],'infoClass',true);
-    //             Vue.set(this.listData[i],'devcountall',devcountall);
-    //             Vue.set(this.listData[i],'isalarmall',isalarmall);
-    //         }else{
-    //             Vue.set(this.listData[i],'infoClass',false);
-    //         }
-    //     }
-    //   }
-
-
-    // })
-
+    created () {
         if(JSON.stringify(localStorage.loginInfo) == undefined){
             this.$message({
                 message: "请登录",
@@ -49,23 +46,43 @@ export default {
             this.$router.push({path:'/login'});
             return;
         }
-
-
-  },
-  mounted() {
-    //首页隐藏滚动
-    indexScroll();
-   
-  },
-  data() {
-    return {
+    },
+    mounted() {
         
+    },
+    data() {
+       return {
+           //侧边点击显示或隐藏
+           navbtn:'open',
+           //侧边头部切换
+           activeName: 'second',
+       }
+   },
+    methods:{
+        //点击隐藏显示侧边
+        navclick(){
+            if(this.navbtn=='open'){
+                $(this.$refs.sidebar).css("left","-225px");
+                $(this.$refs.content).css({
+                    "padding-left":"0",
+                });
+                $(this.$refs.navbtn).addClass("fa-chevron-right");
+                this.navbtn='close';
+            }else{
+                $(this.$refs.sidebar).css("left","0");
+                $(this.$refs.content).css({
+                    "padding-left":"225px",
+                });
+                $(this.$refs.navbtn).removeClass("fa-chevron-right");
+                this.navbtn='open';
+            }
+             
+        },
+
     }
-  },
-  methods:{
-    
-  }
-  
 }
 </script>
+
+
+
 
